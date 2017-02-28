261f5741990ada5cc33387f75b49967180890588
Justin Brevik <justin@justin-laptop>
2017-02-27 19:33:25 -0700

    2/27/17

--- ruby/attr_accessor.rb
+++ ruby/attr_accessor.rb
@@ -3,19 +3,21 @@ class Person
 	def initialize(name, age)
 		@name = name
 		@age = age
+
 	end
 
 	def change(n, a)
-		self.name = n
-		self.age = a
+		self.name 	= n
+		self.age  	= a
+
 	end
 
 	def show_info
 		puts "#{name} is #{age}"
-
 	end
+
 end
 
-	p = Person.new("Justin", 34)
-	p.change("Tom", 28)
-	p.show_info
+p = Person.new("Justin", 34)
+p.change("Tom", 28)
+p.show_info
