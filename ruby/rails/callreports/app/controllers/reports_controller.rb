8322bb5425536a6ee3728fe3b6c38f76e92e5cb8
Justin Brevik <justin@justin-laptop>
2017-04-23 19:58:47 -0700

    4/23/17-2

--- ruby/rails/callreports/app/controllers/reports_controller.rb
+++ ruby/rails/callreports/app/controllers/reports_controller.rb
@@ -27,38 +27,34 @@ class ReportsController < ApplicationController
 
 	def inboundtogroup
 		@brokers = Report.where("called_number = '8662'")
-		.or(Report.where("party_1_name = 'Brokers'"))
-		.where("call_direction = 'I'")
+					 .or(Report.where("party_1_name = 'Brokers'"))
+					 .where("call_direction = 'I'")
 
-		@total = @brokers.count
-		@test = calc_callduration
-
-
-
-=begin
-		sum = 0
-		for x in @duration do
-		x.gsub! ':', ''
-		end
-		for x in 0..@duration.length do
-				sum += @duration[x].to_i
-		end
-
-		@calltotals = sum * 0.6 / 60
-=end
+		@totalcall = @brokers.count
+		@totalduration = calc_callduration
 		@totalring = @brokers.sum(:ring_duration)
+		@totalhold = @brokers.sum(:hold_time)
+		@totalpark = @brokers.sum(:park_time)
+		@avgduration = calc_avgduration
+		@avgring = @brokers.average(:ring_duration)
+		@avghold = @brokers.average(:hold_time)
+		@avgpark = @brokers.average(:park_time)
 
 	end
 
 	private
 	def calc_callduration
-		@duration = @brokers.map {|i| i.call_duration}
-		@strip = @duration.map {|x| x.gsub ':', ''}
-		@converttoi = @strip.map {|x| x.to_i}
-		@calladd = @converttoi.sum
-		@calltotal = @calladd * 0.6 / 60
-		return @calltotal
+		@duration = @brokers.map {|i| i.call_duration.gsub ':', ''}
+		@talktotal = @duration.map.sum {|x| x.to_i}
+		@total = Time.at(@totaltalk).utc.strftime("%H:%M:%S")
+		return @total
 	end
+
+	def calc_avgduration
+
+	end
+
+
 
 
 end
