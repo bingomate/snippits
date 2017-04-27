fc1c810cadc078c8a40eb1681acf47b858857339
Justin Brevik <jbrevik@hsmove.com>
2017-04-26 19:58:52 -0700

    4/27/17

--- ruby/rails/callreports/app/controllers/reports_controller.rb
+++ ruby/rails/callreports/app/controllers/reports_controller.rb
@@ -27,30 +27,42 @@ class ReportsController < ApplicationController
 
 	def inboundtogroup
 		@brokers = Report.where("called_number = '8662'")
+					 .or(Report.where("party_1_device = 'E2114'"))
+					 .or(Report.where("party_1_device = 'E2147'"))
+					 .or(Report.where("party_1_device = 'E2113'"))
 					 .or(Report.where("party_1_name = 'Brokers'"))
 					 .where("call_direction = 'I'")
 
 		@totalcall = @brokers.count
 		@totalduration = calc_callduration
-		@totalring = @brokers.sum(:ring_duration)
-		@totalhold = @brokers.sum(:hold_time)
-		@totalpark = @brokers.sum(:park_time)
+		@totalring = convert_seconds(@brokers.sum(:ring_duration))
+		@totalhold = convert_seconds(@brokers.sum(:hold_time))
+		@totalpark = convert_seconds(@brokers.sum(:park_time))
 		@avgduration = calc_avgduration
-		@avgring = @brokers.average(:ring_duration)
-		@avghold = @brokers.average(:hold_time)
-		@avgpark = @brokers.average(:park_time)
+		@avgring = convert_seconds(@brokers.average(:ring_duration))
+		@avghold = convert_seconds(@brokers.average(:hold_time))
+		@avgpark = convert_seconds(@brokers.average(:park_time))
 
 	end
 
 	private
+
+	def convert_seconds(seconds)
+		Time.at(seconds).utc.strftime("%H:%M:%S")
+	end
+
 	def calc_callduration
-		@duration = @brokers.map {|i| i.call_duration.gsub ':', ''}
-		@talktotal = @duration.map.sum {|x| x.to_i}
-		@total = Time.at(@totaltalk).utc.strftime("%H:%M:%S")
-		return @total
+		@duration = @brokers.map {|i| i.call_duration}
+		@seconds = @duration.map {|x| x.split(':').map {|a| a.to_i}.inject(0) { |a, b| a * 60 + b}}
+		convert_seconds(@seconds.sum)
+
 	end
 
 	def calc_avgduration
+		@avgduration = @brokers.map {|i| i.call_duration}
+		@avgseconds = @avgduration.map {|x| x.split(':').map {|a| a.to_i}.inject(0) { |a, b| a * 60 + b}}
+    @calc_avg = @avgseconds.sum / @avgduration.count
+		convert_seconds(@calc_avg)
 
 	end
 
