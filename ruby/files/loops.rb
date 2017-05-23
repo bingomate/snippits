061b002c75894115ed9aaa73f6100d6fd23798c9
Justin Brevik <justin@justin-laptop>
2017-05-22 19:14:49 -0700

    Adding stuff

---
+++ ruby/files/loops.rb
@@ -0,0 +1,6 @@
+num_list = [2,6,7,4,2,11,16,32]
+
+#total = num_list.inject(0){|sum,x | sum + x }
+#puts total
+
+[1,2,3,4].sum
---
+++ ruby/files/print_middle.rb
@@ -0,0 +1,14 @@
+def get_middle(name)
+  namearr = name.split(//)
+  num = namearr.length
+  result = num/2
+    if num.even?
+      puts "The middle 2 letters of #{name} is #{namearr[result - 1]} and #{namearr[result]}"
+    else
+      puts "The middle letter of #{name} is #{namearr[result]}"
+    end
+
+
+end
+
+get_middle('justini')
